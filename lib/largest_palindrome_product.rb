4fcb98472330dc8c3383794de2c2ecb46e100d79
jequezinho <jroverby92@gmail.com>
2020-09-09 13:05:22 -0500

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,22 @@
-# Implement your procedural solution here!
+def largest_palindrome_product
+    best = 0
+    (100..999).to_a.each { |i| 
+        (100..999).to_a.each { |j| 
+            if is_palindrome(i * j) && i * j > best
+                best = i * j
+            end
+        }
+    }
+    best
+end
+
+def is_palindrome(num)
+    num = num.to_s
+    if num[0] != num[-1]
+        return false
+    elsif num.length > 2
+        return is_palindrome(num[1...-1])
+    else
+        return true
+    end
+end
